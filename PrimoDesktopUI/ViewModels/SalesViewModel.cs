24cdcb281cee08127db430fa1c3544b065c275c8
Ahmed Ibrahim <ahmedibrahim0100@yahoo.com>
2020-02-20 10:02:47 +0200

    Simplify the Tax Calculations
    Converts the tax calculation Foreach over to a LINQ statement

--- PrimoDesktopUI/ViewModels/SalesViewModel.cs
+++ PrimoDesktopUI/ViewModels/SalesViewModel.cs
@@ -211,14 +211,19 @@ namespace PrimoDesktopUI.ViewModels
         {
             decimal taxAmount = 0;
             decimal taxRate = _configHelper.GetTaxRate() / 100;
+            taxAmount = Cart
+                .Where(x => x.Product.IsTaxable)
+                .Sum(x => x.Product.RetailPrice * x.QuantityInCart * taxRate);
 
-            foreach (var item in Cart)
-            {
-                if (item.Product.IsTaxable)
-                {
-                    taxAmount += (item.Product.RetailPrice * item.QuantityInCart * taxRate);
-                }
-            }
+            //Old code using Foreach
+
+            //foreach (var item in Cart)
+            //{
+            //    if (item.Product.IsTaxable)
+            //    {
+            //        taxAmount += (item.Product.RetailPrice * item.QuantityInCart * taxRate);
+            //    }
+            //}
             return taxAmount;
         }
     }
